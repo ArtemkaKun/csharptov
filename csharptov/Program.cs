5c4389fdce9fb63bc9c5d084a71eb14c0e3f2f44
Artem Yurchenko <zumikq78@gmail.com>
2022-03-24 22:01:14 +0100

    Code logic was changed to extend gameplay capabilities

--- csharptov/Program.cs
+++ csharptov/Program.cs
@@ -3,114 +3,102 @@
 public abstract class Entity
 {
 	public readonly string name;
+	
+	protected readonly uint defence;
+	protected readonly float power;
 
-	protected Entity(string name)
+	protected Entity(string name, uint defence, float power)
 	{
 		this.name = name;
-	}
-}
-
-public class Enemy : Entity
-{
-	public readonly uint defence;
-
-	public Enemy(string name, uint defence) : base(name)
-	{
 		this.defence = defence;
+		this.power = power;
 	}
+
+	public abstract float GetAttackPower();
+	public abstract float GetDefencePower();
 }
 
 public abstract class Human : Entity
 {
-	protected uint xp;
+	public uint XP { get; protected set; }
+	public uint WeaponQuality { get; private set; }
 
-	protected readonly uint weaponQuality;
-
-	protected Human(string name, uint xp, uint weaponQuality) : base(name)
+	protected Human(string name, uint defence, float power, uint xp, uint weaponQuality) : base(name, defence, power)
 	{
-		this.xp = xp;
-		this.weaponQuality = weaponQuality;
+		XP = xp;
+		WeaponQuality = weaponQuality;
+	}
+	
+	public override sealed float GetAttackPower()
+	{
+		return power * XP * WeaponQuality;
+	}
+	
+	public override sealed float GetDefencePower()
+	{
+		return defence * XP * WeaponQuality;
 	}
 }
 
 public class Player : Human
 {
-	private readonly float power;
-
-	public Player(string name, uint xp, uint weaponQuality, float power) : base(name, xp, weaponQuality)
-	{
-		this.power = power;
-	}
-
-	public float GetAttackPower()
-	{
-		return power * xp * weaponQuality;
-	}
+	public uint HP { get; private set; } = 100;
+	
+	public Player(string name, uint defence, float power, uint xp, uint weaponQuality) : base(name, defence, power, xp, weaponQuality) { }
 
 	public void IncrementXP()
 	{
-		xp += 1;
+		XP += 1;
+	}
+	
+	public void DecrementHP()
+	{
+		HP -= 1;
 	}
 }
 
-public interface IEnemy
-{
-	public uint GetDefence();
-	public string GetName();
-}
-
-public class Wolf : Enemy, IEnemy
+public class Wolf : Entity
 {
 	private readonly uint agile;
 
-	public Wolf(string name, uint defence, uint agile) : base(name, defence)
+	public Wolf (string name, uint defence, float power, uint agile) : base(name, defence, power)
 	{
 		this.agile = agile;
 	}
 
-	public uint GetDefence()
+	public override float GetAttackPower()
+	{
+		return power * agile;
+	}
+
+	public override float GetDefencePower()
 	{
 		return defence * agile;
 	}
-
-	public string GetName()
-	{
-		return name;
-	}
 }
 
-public class Guard : Human, IEnemy
+public class Guard : Human
 {
-	private readonly Enemy enemyStats;
-
-	public Guard(string name, uint xp, uint weaponQuality, uint defence) : base(name, xp, weaponQuality)
-	{
-		enemyStats = new Enemy(name, defence);
-	}
-
-	public uint GetDefence()
-	{
-		return enemyStats.defence * weaponQuality * xp;
-	}
-
-	public string GetName()
-	{
-		return name;
-	}
+	public Guard (string name, uint defence, float power, uint xp, uint weaponQuality) : base(name, defence, power, xp, weaponQuality) { }
 }
 
 public static class Program
 {
 	private const string HIT_COMMAND = "hit";
+	private const string DEFENCE_COMMAND = "defence";
 	private const string EXIT_COMMAND = "exit";
+	
 	private const string DEFAULT_PLAYER_NAME = "Player";
 	private const uint START_XP = 1;
-	private const uint MIN_START_WEAPON_QUALITY = 1;
+	private const uint MIN_START_WEAPON_QUALITY = 2;
 	private const uint MAX_START_WEAPON_QUALITY = 5;
-	private const uint MAX_ENEMY_DEFENCE = 5;
-	private const uint MAX_AGILE = 10;
-	private const uint MAX_ENEMY_WEAPON_QUALITY = 10;
-	private const uint MAX_ENEMY_XP = 10;
+	private const uint MIN_START_DEFENCE = 2;
+	private const uint MAX_START_DEFENCE = 5;
+	
+	private const uint MAX_DEFAULT_ENEMY_DEFENCE = 5;
+	private const uint MAX_DEFAULT_AGILE = 3;
+	private const uint MAX_DEFAULT_ENEMY_WEAPON_QUALITY = 3;
+	private const uint MAX_DEFAULT_ENEMY_XP = 3;
 
 	private static Player playerInstance = null!;
 
@@ -125,14 +113,16 @@ public static class Program
 	{
 		Console.WriteLine("Tiny RPG");
 		Console.WriteLine($"Print \"{HIT_COMMAND}\" to attack an enemy");
+		Console.WriteLine($"Print \"{DEFENCE_COMMAND}\" to defence enemy's attack and counterattack");
 		Console.WriteLine($"Print \"{EXIT_COMMAND}\" to finish the game");
+		Console.WriteLine();
 	}
 
 	private static void CreatePlayerCharacter()
 	{
 		Console.WriteLine("Name of your character:");
-		playerInstance = new Player(GetPlayerName(), START_XP, GetPlayerStartWeaponQuality(), GetStartPower());
-		Console.WriteLine($"Your character - {playerInstance.name}, attack power - {playerInstance.GetAttackPower()}");
+		playerInstance = new Player(GetPlayerName(), GetPlayerStartDefence(), GetStartPower(), START_XP, GetPlayerStartWeaponQuality());
+		PrintPlayerStatsWithEmptyLine();
 	}
 
 	private static string GetPlayerName()
@@ -140,9 +130,9 @@ public static class Program
 		return Console.ReadLine() ?? DEFAULT_PLAYER_NAME;
 	}
 
-	private static uint GetPlayerStartWeaponQuality()
+	private static uint GetPlayerStartDefence()
 	{
-		return (uint) Random.Shared.Next((int) MIN_START_WEAPON_QUALITY, (int) MAX_START_WEAPON_QUALITY);
+		return (uint) Random.Shared.Next((int) MIN_START_DEFENCE, (int) MAX_START_DEFENCE);
 	}
 
 	private static float GetStartPower()
@@ -150,35 +140,60 @@ public static class Program
 		return Random.Shared.NextSingle() + 1.0f;
 	}
 
+	private static uint GetPlayerStartWeaponQuality()
+	{
+		return (uint) Random.Shared.Next((int) MIN_START_WEAPON_QUALITY, (int) MAX_START_WEAPON_QUALITY);
+	}
+
+	private static void PrintPlayerStatsWithEmptyLine()
+	{
+		Console.WriteLine($"Your character - {playerInstance.name}, attack power - {playerInstance.GetAttackPower()}, defence power - {playerInstance.GetDefencePower()}, XP - {playerInstance.XP}, weapon quality - {playerInstance.WeaponQuality}");
+		Console.WriteLine();
+	}
+
 	private static void StartMainGameLoop()
 	{
 		while (true)
 		{
-			IEnemy currentEnemy = CreateNewEnemy();
-			Console.WriteLine($"Your next enemy - {currentEnemy.GetName()}, defence - {currentEnemy.GetDefence()}");
+			Entity currentEnemy = CreateNewEnemy();
+			PrintEnemyStats(currentEnemy);
+
 			Console.WriteLine("What will you do?");
 			string? userInput = Console.ReadLine();
 
-			if (userInput != null)
+			if (userInput is null or EXIT_COMMAND)
 			{
-				if (userInput == EXIT_COMMAND)
-				{
-					break;
-				}
+				break;
+			}
 
-				if (userInput == HIT_COMMAND)
-				{
+			switch (userInput)
+			{
+				case HIT_COMMAND:
 					TryKillEnemy(currentEnemy);
-				}
+					break;
+				case DEFENCE_COMMAND:
+					TryDefenceEnemyAttack(currentEnemy);
+					break;
+				default:
+					continue;
+			}
+
+			if (playerInstance.HP == 0)
+			{
+				Console.WriteLine($"You are dead. Your XP is - {playerInstance.XP}");
+				Console.WriteLine();
+				Console.WriteLine("Create new character");
+				Console.WriteLine();
+				CreatePlayerCharacter();
 			}
 			else
 			{
-				break;
+				PrintPlayerStatsWithEmptyLine();
 			}
 		}
 	}
 
-	private static IEnemy CreateNewEnemy()
+	private static Entity CreateNewEnemy()
 	{
 		int randomNumber = Random.Shared.Next(2);
 		return randomNumber == 0 ? CreateNewWolf() : CreateNewGuard();
@@ -186,46 +201,93 @@ public static class Program
 
 	private static Wolf CreateNewWolf()
 	{
-		return new Wolf("Wolf", GetRandomDefence(), GetRandomAgile());
+		return new Wolf("Wolf", GetRandomDefence(), GetStartPower(), GetRandomAgile());
 	}
 
 	private static uint GetRandomDefence()
 	{
-		return (uint) Random.Shared.Next((int) MAX_ENEMY_DEFENCE);
+		return (uint) Random.Shared.Next((int) (MAX_DEFAULT_ENEMY_DEFENCE * playerInstance.XP));
 	}
 
 	private static uint GetRandomAgile()
 	{
-		return (uint) Random.Shared.Next((int) MAX_AGILE);
+		return (uint) Random.Shared.Next((int) (MAX_DEFAULT_AGILE * playerInstance.XP));
 	}
 
 	private static Guard CreateNewGuard()
 	{
-		return new Guard("Guard", GetRandomXP(), GetRandomEnemyWeaponQuality(), GetRandomDefence());
+		return new Guard("Guard", GetRandomDefence(), GetStartPower(), GetRandomXP(), GetRandomEnemyWeaponQuality());
 	}
 
 	private static uint GetRandomEnemyWeaponQuality()
 	{
-		return (uint) Random.Shared.Next((int) MAX_ENEMY_WEAPON_QUALITY);
+		return (uint) Random.Shared.Next((int) (MAX_DEFAULT_ENEMY_WEAPON_QUALITY * playerInstance.XP));
 	}
 
 	private static uint GetRandomXP()
 	{
-		return (uint) Random.Shared.Next((int) MAX_ENEMY_XP);
+		return (uint) Random.Shared.Next((int) (MAX_DEFAULT_ENEMY_XP * playerInstance.XP));
 	}
 	
-	private static void TryKillEnemy(IEnemy currentEnemy)
+	private static void PrintEnemyStats (Entity currentEnemy)
 	{
-		bool canKillEnemy = currentEnemy.GetDefence() <= playerInstance.GetAttackPower();
+		Console.WriteLine($"Your next enemy - {currentEnemy.name}, attack = {currentEnemy.GetAttackPower()}, defence - {currentEnemy.GetDefencePower()}");
+		Console.WriteLine();
+	}
+	
+	private static void TryKillEnemy(Entity? currentEnemy)
+	{
+		bool canKillEnemy = currentEnemy?.GetDefencePower() <= playerInstance.GetAttackPower();
 
 		if (canKillEnemy == true)
 		{
 			Console.WriteLine("Enemy was killed!");
-			playerInstance.IncrementXP();
+			IncrementPlayerXPWithMessage();
 		}
 		else
 		{
 			Console.WriteLine($"Your attack is too low ({playerInstance.GetAttackPower()})");
+			DecrementPlayerHPWithMessage();
 		}
+	}
+
+	private static void TryDefenceEnemyAttack(Entity? currentEnemy)
+	{
+		bool canDefence = playerInstance.GetDefencePower() >= currentEnemy?.GetAttackPower();
+
+		if (canDefence == true)
+		{
+			Console.WriteLine("Enemy was killed by counterattack!");
+			IncrementPlayerXPWithMessage();
+		}
+		else
+		{
+			Console.WriteLine($"Your defence is too low ({playerInstance.GetDefencePower()})");
+			DecrementPlayerHPWithMessage();
+		}
+	}
+
+	private static void IncrementPlayerXPWithMessage()
+	{
+		playerInstance.IncrementXP();
+		PrintPlayerGainXPWithEmptyLine();
+	}
+
+	private static void PrintPlayerGainXPWithEmptyLine()
+	{
+		Console.WriteLine($"You gain 1 XP, your actual XP is {playerInstance.XP}");
+		Console.WriteLine();
+	}
+
+	private static void DecrementPlayerHPWithMessage()
+	{
+		playerInstance.DecrementHP();
+		PrintPlayerLostHPWithEmptyLine();
+	}
+
+	private static void PrintPlayerLostHPWithEmptyLine()
+	{
+		Console.WriteLine($"You lost 1 HP, your actual HP is {playerInstance.HP}");
+		Console.WriteLine();
 	}
 }
